75646c75ee1fedae0a5684be3afa2b6ec1da4701
potesuch <potesuch1@yandex.ru>
2024-07-11 17:58:43 +0300

    add docs

--- backend/properties/models.py
+++ backend/properties/models.py
@@ -7,6 +7,23 @@ User = get_user_model()
 
 
 class Property(models.Model):
+    """
+    Модель недвижимости, представляющая собой объект для аренды.
+
+    Attributes:
+        id (UUIDField): Уникальный идентификатор объекта недвижимости.
+        title (CharField): Название объекта недвижимости.
+        description (TextField): Описание объекта недвижимости.
+        price_per_night (IntegerField): Цена за ночь.
+        bedrooms (IntegerField): Количество спален.
+        bathrooms (IntegerField): Количество ванных комнат.
+        country (CharField): Страна расположения объекта.
+        country_code (CharField): Код страны.
+        category (CharField): Категория объекта (например, квартира, дом и т.д.).
+        image (ImageField): Изображение объекта недвижимости.
+        host (ForeignKey): Владелец (хост) объекта, связанный с моделью пользователя.
+        created_at (DateTimeField): Дата и время создания записи.
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -25,6 +42,14 @@ class Property(models.Model):
 
 
 class Favorite(models.Model):
+    """
+    Модель избранного, представляющая связь между пользователем и объектом недвижимости, который пользователь добавил в избранное.
+
+    Attributes:
+        id (UUIDField): Уникальный идентификатор записи избранного.
+        property (ForeignKey): Связь с моделью Property, указывающая на избранный объект недвижимости.
+        user (ForeignKey): Связь с моделью User, указывающая на пользователя, добавившего объект в избранное.
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     property = models.ForeignKey(
         Property, on_delete=models.CASCADE, related_name='in_favorite'
