56eaac5d44cfd7c6d619470bdbe4c4600bf84259
potesuch <potesuch1@yandex.ru>
2024-07-12 12:08:51 +0300

    add property fetch

--- frontend/app/components/properties/PropertyList.tsx
+++ frontend/app/components/properties/PropertyList.tsx
@@ -1,6 +1,29 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
 import PropertyListItem from "./PropertyListItem";
 
 const PropertyList = () => {
+    const getProperties = async () => {
+        const url = 'http://localhost:8000/api/properties/';
+
+        await fetch(url, {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then((json) => {
+                console.log('json', json);
+            })
+            .catch((error) => {
+                console.log('error', error);
+            });
+    };
+    
+    useEffect(() => {
+        getProperties();
+    }, []);
+    
     return (
         <>
             <PropertyListItem />
