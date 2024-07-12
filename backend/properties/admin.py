5be10b8f5ba010f4d15d26b0b9e92b6f38cd2ac7
potesuch <potesuch1@yandex.ru>
2024-07-12 11:21:04 +0300

    add property and category admin

--- backend/properties/admin.py
+++ backend/properties/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Property, Category
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'description',
+        'price_per_night',
+        'category',
+        'host',
+        'created_at'
+    )
+    list_display_links = ('id', 'title')
+    list_filter = ('price_per_night', 'category')
+    search_fields = ('id', 'title', 'description', 'category','host')
+    ordering = ('-created_at',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
