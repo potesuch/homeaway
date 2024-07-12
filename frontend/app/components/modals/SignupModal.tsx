b57341dba0d8f65a79251ffb3a8d6eecf592f7f6
potesuch <potesuch1@yandex.ru>
2024-07-12 18:21:27 +0300

    add signup

--- frontend/app/components/modals/SignupModal.tsx
+++ frontend/app/components/modals/SignupModal.tsx
@@ -1,25 +1,68 @@
 'use client';
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import Modal from "./Modal";
 import useSignupModal from "../hooks/useSignupModal";
 import CustomButton from "../forms/CustomButton";
+import apiService from "@/app/services/apiService";
 
 const SignupModal = () => {
+    const router = useRouter();
     const signupModal = useSignupModal();
+    const [email, setEmail] = useState('');
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [errors, setErrors] = useState<string[]>([]);
+
+    const submitSignup = async () => {
+        const formData = {
+            email: email,
+            password1: password1,
+            password2: password2
+        }
+
+        const response = await apiService.post('/api/auth/register/', JSON.stringify(formData));
+
+        if (response.access) {
+
+            signupModal.close();
+            router.push('/');
+        } else {
+            const tmpErrors: string[] = Object.values(response).map((error: any) => {
+                return error;
+            });
+
+            setErrors(tmpErrors);
+        }
+    }
 
     const content = (
         <>
-            <form action="" className="space-y-4">
-                <input placeholder="Your e-mail address" type="email" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
+            <form
+                action={submitSignup}
+                className="space-y-4"
+            >
+                <input onChange={(e) => setEmail(e.target.value)} placeholder="Your e-mail address" type="email" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
 
-                <input placeholder="Your password" type="password" className="w-full h-[54px] border border-gray-300 rounded-xl px-4" />
-                <input placeholder="Repeat password" type="password" className="w-full h-[54px] border border-gray-300 rounded-xl px-4" />
+                <input onChange={(e) => setPassword1(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] border border-gray-300 rounded-xl px-4" />
+                <input onChange={(e) => setPassword2(e.target.value)} placeholder="Repeat password" type="password" className="w-full h-[54px] border border-gray-300 rounded-xl px-4" />
 
-                <div className="p-5 bg-helio-dark rounded-xl text-white opacity-80">Error message</div>
+                {errors.map((error, index) => {
+                    return (
+                        <div
+                            key={`error_${index}`}
+                            className="p-5 bg-helio-dark rounded-xl text-white opacity-80"
+                        >
+                            {error}
+                        </div>
+                    )
+                })}
 
                 <CustomButton 
                     label='Submit'
-                    onClick={() => {console.log('Clicked')}}
+                    onClick={submitSignup}
                 />
             </form>
         </>
--- frontend/app/services/apiService.ts
+++ frontend/app/services/apiService.ts
@@ -1,5 +1,5 @@
 const apiService = {
-    get: async function (url: string): Promise<any> {
+    get: async function(url: string): Promise<any> {
         console.log('get', url)
 
         return new Promise((resolve, reject) => {
@@ -11,6 +11,29 @@ const apiService = {
                 }
             })
                 .then(response => response.json())
+                .then((json) => {
+                    console.log('Response:', json);
+
+                    resolve(json);
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
+    },
+    post: async function(url: string, data:any): Promise<any> {
+        console.log('post', url, data);
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json'
+                },
+                body: data
+            })
+                .then(response => response.json)
                 .then((json) => {
                     console.log('Response:', json);
 
