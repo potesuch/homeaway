c42515ce72bbf9cf85f458019a39d875534d0738
potesuch <potesuch1@yandex.ru>
2024-07-13 20:28:01 +0300

    add sudmitLogin

--- frontend/app/components/modals/LoginModal.tsx
+++ frontend/app/components/modals/LoginModal.tsx
@@ -1,25 +1,49 @@
 'use client';
 
+import { useState } from "react";
+
 import Modal from "./Modal";
 import useLoginModal from "../hooks/useLoginModal";
 
 import CustomButton from "../forms/CustomButton";
+import apiService from "@/app/services/apiService";
 
 const LoginModal = () => {
     const loginModal = useLoginModal();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const submitLogin = async () => {
+        const formData = {
+            email: email,
+            password: password
+        }
+
+        const response = await apiService.post('/api/auth/jwt/create/', JSON.stringify(formData));
+
+        if (response.access) {
+            //handle login
+            console.log('Logged')
+        } else {
+            setError(response.detail);
+        }
+    }
 
     const content = (
         <>
             <form action="" className="space-y-4">
-                <input placeholder="Your e-mail address" type="email" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
+                <input onChange={(e) => setEmail(e.target.value)} placeholder="Your e-mail address" type="email" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
 
-                <input placeholder="Your password" type="password" className="w-full h-[54px] border border-gray-300 rounded-xl px-4" />
+                <input onChange={(e) => setPassword(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] border border-gray-300 rounded-xl px-4" />
 
-                <div className="p-5 bg-helio-dark rounded-xl text-white opacity-80">Error message</div>
+                {error &&
+                    <div className="p-5 bg-helio-dark rounded-xl text-white opacity-80">{error}</div>
+                }
 
                 <CustomButton 
                     label='Submit'
-                    onClick={() => {console.log('Clicked')}}
+                    onClick={submitLogin}
                 />
             </form>
         </>
