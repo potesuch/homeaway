c74e6387ff41f4b8c73cc7682e1540df5b3f3375
potesuch <potesuch1@yandex.ru>
2024-07-13 17:51:54 +0300

    add simple_jwt settings and djoser urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -12,4 +12,6 @@ router.register('properties', PropertyViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
--- backend/homeaway/settings.py
+++ backend/homeaway/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 from pathlib import Path
 from dotenv import load_dotenv
@@ -153,4 +154,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'UPDATE_LAST_LOGIN': True,
 }
