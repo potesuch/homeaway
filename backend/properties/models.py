3847c261b092571ebe44605ca2fe2a8e058a63cb
potesuch <potesuch1@yandex.ru>
2024-07-14 17:56:52 +0300

    update category model

--- backend/properties/models.py
+++ backend/properties/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
         name (CharField): Название категории.
         image (ImageField): Изображение категории.
     """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
     name = models.CharField(max_length=20)
     image = models.ImageField(upload_to='uploads/categories')
 
