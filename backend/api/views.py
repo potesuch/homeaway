2d36d9dfb1515f33f71964dd5bea2a5c3c70ce19
potesuch <potesuch1@yandex.ru>
2024-07-14 17:56:17 +0300

    update property viewset

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,19 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 
-from .serializers import ShortPropertySerializer
+from .serializers import ShortPropertySerializer, PropertySerializer
 from properties.models import Property
 
 
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
-    serializer_class = ShortPropertySerializer
+    serializer_class = PropertySerializer
     permission_classes = (permissions.AllowAny,)
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ShortPropertySerializer
+        return super().get_serializer_class()
+
+    def perform_create(self, serializer):
+        serializer.save(host=self.request.user)
