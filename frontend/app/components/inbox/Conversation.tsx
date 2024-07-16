875fd8c28d754f4a5642c54c9d52e6ea7f59c442
potesuch <potesuch1@yandex.ru>
2024-07-16 19:04:54 +0300

    add conversation page dynamic

--- frontend/app/components/inbox/Conversation.tsx
+++ frontend/app/components/inbox/Conversation.tsx
@@ -1,9 +1,30 @@
-const Conversation = () => {
+import { useRouter } from "next/navigation";
+
+import { User } from "../hooks/useUser";
+import { ConversationType } from "@/app/inbox/page";
+
+interface ConversationProps {
+    user: User;
+    conversation: ConversationType;
+}
+
+const Conversation: React.FC<ConversationProps> = ({
+    user,
+    conversation
+}) => {
+    const router = useRouter();
+    const otherUser = conversation.users.find((conv_user) => conv_user.id != user.id)
+    
     return (
         <div className="cursor-pointer px-6 py-4 border border-gray-300 rounded-xl">
-            <p className="mb-6 text-xl">John Doe</p>
+            <p className="mb-6 text-xl">{otherUser?.name}</p>
 
-            <p className="text-helio-dark">Go to conversation</p>
+            <p
+                onClick={() => router.push(`/inbox/${conversation.id}/`)}
+                className="text-helio-dark"
+            >
+                Go to conversation
+            </p>
         </div>
     );
 };
--- frontend/app/inbox/page.tsx
+++ frontend/app/inbox/page.tsx
@@ -1,13 +1,52 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
 import Conversation from "../components/inbox/Conversation";
+import useUser, { User } from "../components/hooks/useUser";
+import apiService from "../services/apiService";
+
+export type ConversationType = {
+    id: string;
+    users: User[];
+}
 
 const InboxPage = () => {
+    const [conversations, setConversations] = useState<ConversationType[]>([]);
+
+    const { user } = useUser();
+    
+    const getConversations = async () => {
+        const tmpConversations = await apiService.get('/api/auth/users/me/conversations/');
+        
+        setConversations(tmpConversations);
+    }
+    
+    useEffect(() => {
+        getConversations();
+    }, [])
+    
+    if (!user) {
+        return (
+            <main className="max-w-[1500px] max-auto px-6 py-12">
+                <p>You need to be authenticated...</p>
+            </main>
+        )
+    }
+    
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
             <h1 className="my-6 text-2xl">Inbox</h1>
 
-            <Conversation />
-            <Conversation />
-            <Conversation />
+            {conversations.map((conversation: ConversationType) => {
+                return (
+                    <Conversation
+                        key={conversation.id}
+                        user={user}
+                        conversation={conversation}
+                    />
+                )
+            })}
         </main>
     );
 };
