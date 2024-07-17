6edbeeba4696d6fe1c79af5c7bcc932fe8a30aa3
potesuch <potesuch1@yandex.ru>
2024-07-17 13:08:54 +0300

    add getConversation

--- frontend/app/inbox/[id]/page.tsx
+++ frontend/app/inbox/[id]/page.tsx
@@ -1,6 +1,37 @@
-import ConversationDetail from "@/app/components/inbox/ConversationDetail";
+'use client';
 
-const ConversationPage = () => {
+import useUser, { User } from "@/app/components/hooks/useUser";
+import ConversationDetail from "@/app/components/inbox/ConversationDetail";
+import apiService from "@/app/services/apiService";
+import { useState } from "react";
+
+export type MessageType = {
+    id: string;
+    name: string;
+    body: string;
+    conversationId: string;
+    sent_to: User;
+    sent_from: User;
+}
+
+const ConversationPage = ({ params }: { params: {id: string}}) => {
+    const { user } = useUser();
+    const [conversation, setConversation] = useState();
+
+    const getConversation = async () => {
+        const tmpConversation = await apiService.get(`/api/chat/${params.id}/`);
+
+        setConversation(tmpConversation);
+    }
+
+    if (!user) {
+        return (
+            <main className="max-w-[1500px] max-auto px-6 py-12">
+                <p>You need to be authenticated...</p>
+            </main>
+        )
+    }
+
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
             <ConversationDetail />
